057aa93a926064366beb016752331ccf6f8a626c
user-64bit <arth.prajapati@outlook.com>
2024-10-25 15:58:45 +0530

    hero section

---
+++ components/Header.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+export const Header = () => {
+  return (
+    <div className="flex justify-start items-center gap-x-4">
+      <div role="button">
+        <Image
+          src={"/logo.png"}
+          width={60}
+          height={60}
+          alt="Athena"
+          className="object-cover"
+        />
+      </div>
+      <div
+        role="button"
+        className="border py-4 px-5">
+        <h1>Learn</h1>
+      </div>
+      <div
+        role="button"
+        className="border py-4 px-5">
+        <h1>Join</h1>
+      </div>
+      <div
+        role="button"
+        className="border py-4 px-5">
+        <h1>Build</h1>
+      </div>
+      <div
+        role="button"
+        className="border py-4 px-5">
+        <h1>Contact</h1>
+      </div>
+    </div>
+  )
+};

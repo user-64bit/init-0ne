3e22f72caae0270116df3d3a4a40ed7550e6d95b
user-64bit <arth.prajapati@outlook.com>
2024-10-29 01:22:05 +0530

    wip: send sol

--- app/(routes)/our-team/page.tsx
+++ app/(routes)/our-team/page.tsx
@@ -1,6 +1,7 @@
 import { getGithubData } from "@/actions/getGithubData";
 import { Appbar } from "@/components/AppBar";
 import { TeamGrid } from "@/components/TeamGrid";
+import { Button } from "@/components/ui/button";
 
 const githubIds = ["user-64bit", "hkirat", "torvalds"];
 
@@ -9,6 +10,13 @@ export default async function OurTeamPage() {
   return (
     <div>
       <Appbar title="Our Team" />
+      <Button
+        variant={"ghost"}
+        className="border float-end mb-2 mx-16 font-sans"
+      >
+        Send 0.5 SOL & Support us
+      </Button>
+      <div className="clear-both border-b border-b-slate-400/20"></div>
       <TeamGrid team={team} />
     </div>
   );
